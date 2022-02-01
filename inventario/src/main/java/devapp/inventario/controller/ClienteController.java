ae1948d2ecb474a2afbca0ea3f6c7ffe2776d10c
luisedr98 <luisdominguez818@gmail.com>
2022-01-31 21:04:03 -0500

    registro del cliente y codificacion de la contraseña

--- inventario/src/main/java/devapp/inventario/controller/ClienteController.java
+++ inventario/src/main/java/devapp/inventario/controller/ClienteController.java
@@ -1,16 +1,40 @@
 package devapp.inventario.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import devapp.inventario.entities.Cliente;
+import devapp.inventario.repositories.ClienteRepository;
 
 @Controller
 @RequestMapping("/cliente")
 public class ClienteController 
 {
+
+    @Autowired
+    private BCryptPasswordEncoder BCrypt;
+
+    @Autowired
+    ClienteRepository clienterepository;
+
+    @Autowired
+
     @GetMapping("/carrito") 
     public String cart()
     {
         return "cliente/carrito";
+    }
+
+    @RequestMapping("/save")
+    public String save(Cliente cliente){
+        String pass = cliente.getPassword(); //estrae la contraseña sola
+        cliente.setEstado(1);
+        cliente.setPassword(BCrypt.encode(pass)); //codifica la contraseña
+        cliente.setCelular("sin definir xd");
+        clienterepository.save(cliente);
+        return "redirect:/login";
     }
 }
