6e27a910905d028fabee758c8408b8b4292363ca
SamuelParrales <parrales.samuel@hotmail.com>
2022-02-06 18:01:26 -0500

    Reportes del producto terminado.

--- inventario/src/main/java/devapp/inventario/controller/EmpleadoController.java
+++ inventario/src/main/java/devapp/inventario/controller/EmpleadoController.java
@@ -7,29 +7,44 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import javax.validation.constraints.Null;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import ch.qos.logback.core.joran.conditional.ElseAction;
+
+import devapp.inventario.entities.Categoria;
 import devapp.inventario.entities.Cliente;
+import devapp.inventario.entities.Producto;
+import devapp.inventario.entities.Proveedor;
 import devapp.inventario.entities.RecepPrest;
+import devapp.inventario.repositories.CategoriaRepository;
 import devapp.inventario.repositories.ClienteRepository;
+import devapp.inventario.repositories.ProductoRepository;
+import devapp.inventario.repositories.ProveedorRepository;
 import devapp.inventario.repositories.RecepPrestRepository;
+import devapp.inventario.services.PictureService;
 import devapp.inventario.services.RecepPrestService;
 
 @Controller
 @RequestMapping("/empleado")
 public class EmpleadoController {
+    @Autowired
+    PictureService picture;
     @Autowired
     RecepPrestService recepPrestService;
 
@@ -37,7 +52,16 @@ public class EmpleadoController {
     RecepPrestRepository recepPrestRepo;
 
     @Autowired
+    ProductoRepository produtoRepo;
+
+    @Autowired
     ClienteRepository clienteRepo;
+
+    @Autowired
+    ProveedorRepository provedorRepo;
+
+    @Autowired
+    CategoriaRepository categoriaRepo;
 
     @GetMapping("/menu") 
     public String menu()
@@ -231,5 +255,234 @@ public class EmpleadoController {
         
         model.addAttribute("reservacion", recepPrestRepo.findById(id).get());
         return "factura_recep_prest";
+    }
+
+    // Productos
+    @GetMapping("/producto/add")
+    public String registroProducto(Model model)
+    {
+
+        model.addAttribute("proveedores", provedorRepo.findAll());
+        model.addAttribute("categorias", categoriaRepo.findAll());
+        return "empleado/registrar_productos";
+    }
+
+    @PostMapping("/producto")
+    public String registrarProducto(Model model,
+    @RequestParam String nombre,
+    @RequestParam(defaultValue = "") String descripcion,
+    @RequestParam Double valoru,
+    @RequestParam Double valorp,
+    @RequestParam Integer cantd,
+    @RequestParam Integer categoria,
+    @RequestParam(required = false,defaultValue = "") Integer[] proveedores,
+    @RequestParam MultipartFile file)
+    {
+        
+  
+        //Busqueda de los proveedores
+        List<Proveedor> listProveedores = new ArrayList<Proveedor>();
+        int loops = proveedores.length;
+        for(int i=0;i<loops;i++)
+        {
+            Proveedor proveedor = provedorRepo.findById(proveedores[i]).get();
+            listProveedores.add(proveedor);
+        }
+        //Busqueda de la categoria
+        Categoria cate = categoriaRepo.findById(categoria).get();
+
+
+        //Generación del producto
+        Producto producto = new Producto();
+
+        producto.setNombre(nombre);
+        producto.setDescripcion(descripcion);
+        producto.setValorUnitario(valoru);
+        producto.setValorPrestacion(valorp);
+        producto.setCantDisponible(cantd);
+        producto.setCategoria(cate);
+        producto.setEstado(1);
+        producto.setProveedores(listProveedores);
+        //Guardado de la imagen
+        UUID idPic = UUID.randomUUID();
+        picture.uploadPicture(file, idPic);
+        producto.setImg(idPic);
+        produtoRepo.save(producto);
+        return "redirect:/empleado/producto";
+    }
+
+    //Para el reporte de los productos
+    @GetMapping("/producto")
+    public String reporteProducto(
+        @RequestParam(required = false, defaultValue = "") String nombre,
+        @RequestParam(required = false, defaultValue = "") Integer proveedor,
+        @RequestParam(required = false, defaultValue = "") Integer categoria,
+        @RequestParam(required = false,defaultValue = "1") Integer pag,
+        Model model)
+    {
+        int tamPag = 10;
+        Long cantInstancias=(long) 0;
+        Pageable p = PageRequest.of(pag-1, tamPag);
+        List<Producto> productos =null;
+        Boolean consultado = false;
+        
+        if(!nombre.equals(""))
+        {
+            
+            if(proveedor!=null&&categoria!=null)
+            {
+         
+                productos = produtoRepo.filterByNombreFindCategoria_IdAndProveedor_Id(nombre, categoria, proveedor, p);
+                cantInstancias = produtoRepo.countFilterByNombreFindCategoria_IdAndProveedor_Id(nombre,categoria,proveedor);
+                consultado = true;
+            }
+
+            if(!consultado &&categoria!=null)
+            {
+                productos = produtoRepo.filterByNombreFindCategoria_Id(nombre, categoria, p);
+                cantInstancias = produtoRepo.countFilterByNombreFindCategoria_Id(nombre, categoria);
+                consultado = true;
+            }
+
+            if(!consultado && proveedor!=null) 
+            {
+                productos = produtoRepo.filterByNombreFindProveedor_Id(nombre, proveedor, p);
+                cantInstancias = produtoRepo.countFilterByNombreFindProveedor_Id(nombre, proveedor);       
+                consultado = true;
+            }
+
+            if(!consultado)
+            {
+                productos = produtoRepo.filterByNombre(nombre, p);
+                cantInstancias = produtoRepo.countFilterByNombre(nombre);
+                consultado = true;
+            }
+
+            if(consultado)
+                model.addAttribute("productos", productos);
+        }
+
+        if(!consultado)
+        {
+            
+            if(proveedor!=null&&categoria!=null)
+            {
+                Categoria cate = categoriaRepo.findById(categoria).get();
+                Proveedor prov = provedorRepo.findByIdAndEstado(proveedor, 1);
+                productos = produtoRepo.findAllByCategoriaAndProveedoresAndEstado(cate, prov, 1, p);
+                cantInstancias = produtoRepo.countByEstadoAndCategoriaAndProveedores(1, cate, prov);
+                consultado = true;
+            }
+
+            if(!consultado && proveedor!=null)
+            {
+                Proveedor prov = provedorRepo.findByIdAndEstado(proveedor, 1);
+                productos = produtoRepo.findAllByProveedoresAndEstado(prov, 1, p);
+                cantInstancias = produtoRepo.countByEstadoAndProveedores(1, prov);
+                consultado = true;
+            }
+            if(!consultado&&categoria!=null)
+            {
+                Categoria cate = categoriaRepo.findById(categoria).get();
+                productos = produtoRepo.findAllByCategoriaAndEstado(cate, 1, p);
+                cantInstancias = produtoRepo.countByEstadoAndCategoria(1, cate);
+                consultado = true;
+            }
+
+            if(consultado)
+                model.addAttribute("productos", productos);
+        }
+        if(!consultado)
+        {
+            model.addAttribute("productos",produtoRepo.findAllByEstado(1, p));
+            cantInstancias = produtoRepo.countByEstado(1);
+        }
+
+        //Cantidad de paginas
+        double cantPagina = ((double) cantInstancias/(double) tamPag);
+		int cantPaginaRex = (int) Math.ceil(cantPagina);
+
+        //Cantidad de paginas
+		if(cantPaginaRex<cantPagina)
+            model.addAttribute("cantPag", cantPaginaRex+1);
+        else    
+            model.addAttribute("cantPag", cantPaginaRex);
+
+        model.addAttribute("pageActive", pag);
+        model.addAttribute("proveedores", provedorRepo.findAll());
+        model.addAttribute("categorias", categoriaRepo.findAll());
+
+        return "empleado/search_productos";
+    }
+
+ 
+    @GetMapping("/producto/{id}")
+    public String getProducto(Model model, @PathVariable("id") Integer id )
+    {
+
+        model.addAttribute("proveedores", provedorRepo.findAll());
+        model.addAttribute("categorias", categoriaRepo.findAll());
+        model.addAttribute("producto", produtoRepo.findById(id).get());
+        return "empleado/registrar_productos"; 
+    }
+
+    @PostMapping("/producto/{id}")
+    String updateProducto(Model model,
+    @PathVariable("id") Integer id,
+    @RequestParam String nombre,
+    @RequestParam(defaultValue = "") String descripcion,
+    @RequestParam Double valoru,
+    @RequestParam Double valorp,
+    @RequestParam Integer cantd,
+    @RequestParam Integer categoria,
+    @RequestParam(required = false,defaultValue = "") Integer[] proveedores,
+    @RequestParam (required = false,defaultValue = "") MultipartFile file)
+    {
+        //Busqueda el producto a actualizar
+        Producto producto = produtoRepo.findById(id).get();
+
+        
+                //Busqueda de los proveedores
+        List<Proveedor> listProveedores = new ArrayList<Proveedor>();
+        int loops = proveedores.length;
+        for(int i=0;i<loops;i++)
+        {
+            Proveedor proveedor = provedorRepo.findById(proveedores[i]).get();
+            listProveedores.add(proveedor);
+        }
+
+        //Busqueda de la categoria
+        Categoria cate = categoriaRepo.findById(categoria).get();
+
+        producto.setNombre(nombre);
+        producto.setDescripcion(descripcion);
+        producto.setValorUnitario(valoru);
+        producto.setValorPrestacion(valorp);
+        producto.setCantDisponible(cantd);
+        producto.setCategoria(cate);
+        producto.setEstado(1);
+        producto.setProveedores(listProveedores);
+        //Guardado de la imagen
+        if(!file.isEmpty())
+        {
+            System.out.println("aaaaaaaaa");
+            
+            UUID idPic = UUID.randomUUID();
+            picture.uploadPicture(file, idPic);
+            producto.setImg(idPic);
+        }
+  
+        produtoRepo.save(producto);
+        return "redirect:/empleado/producto";
+    }
+
+    @DeleteMapping("/producto/{id}")
+    public ResponseEntity<Integer> deleteProducto(@PathVariable("id") Integer id)
+    {
+        Producto producto = produtoRepo.findById(id).get();
+        producto.setEstado(0);
+        produtoRepo.save(producto);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+        
     }
 }
--- inventario/src/main/java/devapp/inventario/repositories/ProductoRepository.java
+++ inventario/src/main/java/devapp/inventario/repositories/ProductoRepository.java
@@ -25,7 +25,26 @@ public interface ProductoRepository  extends PagingAndSortingRepository<Producto
     nativeQuery = true)
     public Long countFilterByNombreFindCategoria_Id(@Param(value = "nombre") String nombre, @Param(value = "id") Integer id);
 
-        //Para la paginacion
+    @Query(value = "SELECT COUNT(*) FROM PRODUCTO p "+
+    "WHERE UPPER(p.NOMBRE) "+
+    "LIKE UPPER(concat('%',:nombre,'%')) "+
+     "and estado=1 "+ 
+     "and id_producto in "+ 
+     "(select id_producto from PRODUCTO_PROV "+ 
+    "where id_proveedor=:proveedor)", nativeQuery = true)
+    public Long countFilterByNombreFindProveedor_Id(String nombre,int proveedor);
+
+    @Query(value = "SELECT COUNT(*) FROM PRODUCTO p "+
+    "WHERE UPPER(p.NOMBRE) "+ 
+    "LIKE UPPER(concat('%',:nombre,'%')) "+ 
+     "and estado=1 "+ 
+     "and id_categoria=:categoria "+
+     "and id_producto in "+ 
+     "(select id_producto from PRODUCTO_PROV "+ 
+      "where id_proveedor=:proveedor)",nativeQuery = true)
+    public Long countFilterByNombreFindCategoria_IdAndProveedor_Id(String nombre,Integer categoria, Integer proveedor);
+    
+    //Para la paginacion
     @Query(value = "SELECT * FROM PRODUCTO p WHERE UPPER(p.NOMBRE) LIKE UPPER(concat('%',:nombre,'%')) and estado=1",
     nativeQuery = true)
     public List<Producto> filterByNombre( String nombre,Pageable p);
@@ -34,19 +53,44 @@ public interface ProductoRepository  extends PagingAndSortingRepository<Producto
     nativeQuery = true)
     public List<Producto> filterByNombreFindCategoria_Id(String nombre,int id, Pageable p);
 
+
     
+    @Query(value = "SELECT * FROM PRODUCTO p "+
+    "WHERE UPPER(p.NOMBRE) "+ 
+    "LIKE UPPER(concat('%',:nombre,'%')) "+ 
+     "and estado=1 "+ 
+     "and id_producto in " +
+     "(select id_producto from PRODUCTO_PROV "+ 
+      "where id_proveedor=:proveedor)",nativeQuery = true)
+    public List<Producto> filterByNombreFindProveedor_Id(String nombre,int proveedor, Pageable p);
+
+    
+    @Query(value = "SELECT * FROM PRODUCTO p "+
+    "WHERE UPPER(p.NOMBRE) "+ 
+    "LIKE UPPER(concat('%',:nombre,'%')) "+ 
+     "and estado=1 "+ 
+     "and id_categoria=:categoria "+
+     "and id_producto in "+ 
+     "(select id_producto from PRODUCTO_PROV "+ 
+      "where id_proveedor=:proveedor)",nativeQuery = true)
+      public List<Producto> filterByNombreFindCategoria_IdAndProveedor_Id(String nombre,int categoria,int proveedor, Pageable p);
     
     
     //Consultas usando jpa
     public List<Producto> findAllByEstado(int estado);
     public List<Producto> findAllByEstadoAndCategoria_nombre(int estado,String categoria);
+    public Long countByEstadoAndCategoriaAndProveedores(int estado,Categoria categoria, Proveedor proveedor);
     public Long countByEstadoAndCategoria(int estado,Categoria categoria); 
+    public Long countByEstadoAndProveedores(int estado, Proveedor proveedor);
     public Long countByEstado(int estado);
+     
     //Para la paginacion
     public List<Producto> findAllByEstado(int estado,Pageable p); 
+    public List<Producto> findAllByCategoriaAndEstado(Categoria categoria, int estado, Pageable p);
+    public List<Producto> findAllByCategoriaAndProveedoresAndEstado(Categoria categoria, Proveedor proveedor,int estado, Pageable p);
     public List<Producto> findAllByEstadoAndCategoria_nombre(int estado,String categoria,Pageable p);
-    public List<Producto> findAllByProveedores(Proveedor proveedor,Pageable p);//Falta probar
-
+    public List<Producto> findAllByProveedoresAndEstado(Proveedor proveedor,int estado,Pageable p);//Falta probar
+    
 //
 
 }
